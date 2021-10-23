ecfe2bb839368a41455b0965e225f496b8a68407
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-10-23 21:32:34 +0300

    Добавьте файлы проекта.

---
+++ dz7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            GetVS(a, out S, out V);
+            Console.WriteLine("Площадь стороны куба равна {0} ", S);
+            Console.WriteLine("Объем  куба равен {0} ", V);
+            Console.ReadKey();
+        }
+        static void GetVS(int a, out double S, out double V)
+        {
+            S = a * a;
+            V = a * a * a;
+        }
+
+    }
+}
---
+++ z1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны треугольника 1");
+            double a = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToInt32(Console.ReadLine());
+            double c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стороны треугольника 2");
+            double e = Convert.ToInt32(Console.ReadLine());
+            double f = Convert.ToInt32(Console.ReadLine());
+            double g = Convert.ToInt32(Console.ReadLine());
+            double S, p, S1, S2;
+            GetSp(a, b, c, out S, out p);
+            S1 = S;
+            Console.WriteLine("Площадь первого треугольника S1 {0}. полуперниметр равен {1}", S1, p);
+            GetSp(e, f, g, out S, out p);
+            S2 = S;
+            Console.WriteLine("Площадь второго треугольника S2 {0}. полуперниметр равен {1}", S2, p);
+            if (S1 > S2)
+            {
+                Console.WriteLine("Площадь первого тругольника большe");
+            }
+            else if (S1 == S2)
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+            else
+            {
+                Console.WriteLine("Площади второго треугольника больше");
+            }
+        Console.ReadKey();
+        }
+        static void GetSp(double a, double b, double c, out double S, out double p)
+        {
+            p = ((a + b + c) / 2);
+            S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
